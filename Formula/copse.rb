39a49a3fc758d4cedd6da2bc13215d8f5353f1a8
daiwahome <20108554+daiwahome@users.noreply.github.com>
2026-03-26 16:41:07 +0000

    copse 0.1.3

--- Formula/copse.rb
+++ Formula/copse.rb
@@ -1,7 +1,7 @@
 class Copse < Formula
   desc "TUI for running Claude Code tasks in parallel using git worktrees"
   homepage "https://github.com/daiwahome/copse"
-  url "https://github.com/daiwahome/copse.git", tag: "v0.1.2"
+  url "https://github.com/daiwahome/copse.git", tag: "v0.1.3"
   license "MIT"
 
   bottle do
