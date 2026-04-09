5dc15d68221c7d3c47a8c75d709e429ee096275f
daiwahome <20108554+daiwahome@users.noreply.github.com>
2026-04-09 01:46:13 +0000

    copse: update 0.4.1 bottle.

--- Formula/copse.rb
+++ Formula/copse.rb
@@ -5,11 +5,11 @@ class Copse < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/daiwahome/homebrew-copse/releases/download/copse-0.4.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "59be1b3b0ce672bd5a2d98ccbc04b380a0eabd622a6afdcd6506797b77e6d9f3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "52e62d7dcf78236ad882530b7421170c7e43801b43333d9152657b176e96ea0f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ba45e2999a1219892a259da93b586503a4eb7a6c39fb0c616696f17ae0105f9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a9826eb6eebef543f20ff555e40d8c0563c3914446a7670908fabc95d86df8aa"
+    root_url "https://github.com/daiwahome/homebrew-copse/releases/download/copse-0.4.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6a3482d62c64873acbc4fdda090474bacbcb73b6454566184d3ba44444c7f5ea"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f06b54cdd954f98a2b735aaa733d5dc54c93f89d0bd8aedd5b7e60ac372a408a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9df4ecf498d566d91cfcb013f962d7cf872ff82c87e375029b0e22d5302b5872"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bb87b45cb440d3b09158821cbbc50e3776471b3c88d69a261f6463299e980c41"
   end
 
   depends_on "rust" => :build
